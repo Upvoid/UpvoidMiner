f04ef1f916bf3096c1da441c5aad64b8b4b3bb69
Lukas Boersma <lukas.boersma@rwth-aachen.de>
2013-07-14 21:03:17 +0200

    Initial version of UpvoidMiner

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-UpvoidMiner
+Upvoid Miner
 ===========
 
-Upvoid Miner is a techdemo game to showcase the capabilities of the Upvoid Engine and to provide a starting point for other developers interested in using the engine.
+Upvoid Miner is a techdemo game to showcase the capabilities of the [Upvoid Engine](http://upvoid.com/about) and to provide a starting point for other developers interested in using the engine. The engine it is based on is not yet released, so you can not play it yet. We will release a free playable version as soon as it is ready.
+
+
+License, Copyright and Contact
+===========
+
+The game is open source, licensed under the GPLv3. See LICENSE.txt for the full license text. Upvoid Miner is developed by [Upvoid Studios](http://upvoid.com), the project is hosted at [GitHub](http://github.com/Upvoid/Upvoid-Miner). For any questions, visit those websites or write to mail@upvoid.com.
---
+++ Scripts/HostScript.cs
@@ -0,0 +1,89 @@
+using System;
+using Engine;
+using Engine.Model;
+using Engine.Modding;
+using Engine.Resources;
+using Engine.Scripting;
+using Engine.Rendering;
+using Engine.Physics;
+using Engine.Input;
+using System.Collections.Generic;
+
+namespace UpvoidMiner
+{
+	/// <summary>
+	/// Implements a world generator to create a basic world with some vegetation.
+	/// </summary>
+	public class UpvoidMinerWorldGenerator : SimpleWorldGenerator
+	{
+		TerrainMaterial MatGround;
+
+		/// <summary>
+		/// Initializes the terrain materials and settings.
+		/// </summary>
+        public override bool init()
+        {
+            World world = World;
+            TerrainEngine terr = world.Terrain;
+
+			// Aor now, register a single ground material.
+            MatGround = terr.RegisterMaterial("Ground");
+
+			// Add the geometry for the terrain LoDs >= 9.
+            int pipeline = MatGround.AddDefaultPipeline(9);
+            MatGround.AddDefaultShadowAndZPre(pipeline);
+            MatGround.AddMeshMaterial(pipeline, "Output", Resources.UseMaterial("::Terrain/GrassyMountains", HostScript.ModDomain), Renderer.Opaque.Mesh);
+
+			// Add the geometry for the terrain LoDs 5-8. Add some tree impostors to make the ground look nicer.
+            pipeline = MatGround.AddPipeline(Resources.UseGeometryPipeline("PineImpostorField", HostScript.ModDomain), "Input", 5, 8);
+            MatGround.AddDefaultShadowAndZPre(pipeline, "Input");
+            MatGround.AddMeshMaterial(pipeline, "Input", Resources.UseMaterial("::Terrain/GrassyMountains", HostScript.ModDomain), Renderer.Opaque.Mesh);
+            MatGround.AddMeshMaterial(pipeline, "PineSpawns", Resources.UseMaterial("PineImpostor", HostScript.ModDomain), Renderer.Opaque.Mesh);
+
+			// For terrain LoDs 0-4, spawn "real" tree models instead of the impostors.
+            pipeline = MatGround.AddPipeline(Resources.UseGeometryPipeline("PineField", HostScript.ModDomain), "Input", 0, 4);
+            MatGround.AddDefaultShadowAndZPre(pipeline, "Decimate");
+            MatGround.AddMeshMaterial(pipeline, "Decimate", Resources.UseMaterial("::Terrain/GrassyMountains", HostScript.ModDomain), Renderer.Opaque.Mesh);
+            MatGround.AddMeshMaterial(pipeline, "PineSpawns", Resources.UseMaterial("PineLeaves", HostScript.ModDomain), Renderer.Opaque.Mesh);
+            MatGround.AddMeshMaterial(pipeline, "PineSpawns", Resources.UseMaterial("::Shadow", HostScript.ModDomain), Renderer.Shadow.Mesh);
+
+            return base.init();
+        }
+
+		/// <summary>
+		/// Creates the CSG node network for the terrain generation.
+		/// </returns>
+		public override CsgNode createTerrain()
+        {
+			// Load and return a CsgNode based on the "Hills" expression resource. This will create some generic perlin-based hills.
+            CsgOpUnion union = new CsgOpUnion();
+			ExpressionResource expression = Resources.UseExpression("Hills", HostScript.ModDomain);
+            union.AddNode(new CsgExpression(MatGround.MaterialIndex, expression));
+            return union;
+        }
+
+	}
+
+	/// <summary>
+	/// Main class for the host script.
+	/// </summary>
+	public class HostScript
+	{
+		public static Module Mod;
+		public static ResourceDomain ModDomain;
+
+		/// <summary>
+		/// Starts 
+		/// </summary>
+		public static void Startup(IntPtr _unmanagedModule)
+		{
+			// Get and save the resource domain of the mod, needed for loading resources.
+            Mod = Module.FromHandle(_unmanagedModule);
+            ModDomain = Mod.ResourceDomain;
+
+			// Create the world. Multiple worlds could be created here, but we only want one.
+			// Use the UpvoidMinerWorldGenerator, which will create a simple terrain with some vegetation.
+			Universe.CreateWorld("UpvoidMinerWorld", new UpvoidMinerWorldGenerator());
+		}
+	}
+}
