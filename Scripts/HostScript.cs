ce91a88aaac8692b66ef6ed2c98e946732aed328
Lukas Boersma <lukas.boersma@rwth-aachen.de>
2013-07-17 18:20:17 +0200

    set and applied code formatting rules for script files

--- Scripts/HostScript.cs
+++ Scripts/HostScript.cs
@@ -21,46 +21,44 @@ namespace UpvoidMiner
 		/// <summary>
 		/// Initializes the terrain materials and settings.
 		/// </summary>
-        public override bool init()
-        {
-            World world = World;
-            TerrainEngine terr = world.Terrain;
+		public override bool init()
+		{
+			World world = World;
+			TerrainEngine terr = world.Terrain;
 
-			// Aor now, register a single ground material.
-            MatGround = terr.RegisterMaterial("Ground");
+			// For now, register a single ground material.
+			MatGround = terr.RegisterMaterial("Ground");
 
 			// Add the geometry for the terrain LoDs >= 9.
-            int pipeline = MatGround.AddDefaultPipeline(9);
-            MatGround.AddDefaultShadowAndZPre(pipeline);
-            MatGround.AddMeshMaterial(pipeline, "Output", Resources.UseMaterial("::Terrain/GrassyMountains", HostScript.ModDomain), Renderer.Opaque.Mesh);
+			int pipeline = MatGround.AddDefaultPipeline(9);
+			MatGround.AddDefaultShadowAndZPre(pipeline);
+			MatGround.AddMeshMaterial(pipeline, "Output", Resources.UseMaterial("::Terrain/GrassyMountains", HostScript.ModDomain), Renderer.Opaque.Mesh);
 
 			// Add the geometry for the terrain LoDs 5-8. Add some tree impostors to make the ground look nicer.
-            pipeline = MatGround.AddPipeline(Resources.UseGeometryPipeline("PineImpostorField", HostScript.ModDomain), "Input", 5, 8);
-            MatGround.AddDefaultShadowAndZPre(pipeline, "Input");
-            MatGround.AddMeshMaterial(pipeline, "Input", Resources.UseMaterial("::Terrain/GrassyMountains", HostScript.ModDomain), Renderer.Opaque.Mesh);
-            MatGround.AddMeshMaterial(pipeline, "PineSpawns", Resources.UseMaterial("PineImpostor", HostScript.ModDomain), Renderer.Opaque.Mesh);
+			pipeline = MatGround.AddPipeline(Resources.UseGeometryPipeline("PineImpostorField", HostScript.ModDomain), "Input", 5, 8);
+			MatGround.AddDefaultShadowAndZPre(pipeline, "Input");
+			MatGround.AddMeshMaterial(pipeline, "Input", Resources.UseMaterial("::Terrain/GrassyMountains", HostScript.ModDomain), Renderer.Opaque.Mesh);
+			MatGround.AddMeshMaterial(pipeline, "PineSpawns", Resources.UseMaterial("PineImpostor", HostScript.ModDomain), Renderer.Opaque.Mesh);
 
 			// For terrain LoDs 0-4, spawn "real" tree models instead of the impostors.
-            pipeline = MatGround.AddPipeline(Resources.UseGeometryPipeline("PineField", HostScript.ModDomain), "Input", 0, 4);
-            MatGround.AddDefaultShadowAndZPre(pipeline, "Decimate");
-            MatGround.AddMeshMaterial(pipeline, "Decimate", Resources.UseMaterial("::Terrain/GrassyMountains", HostScript.ModDomain), Renderer.Opaque.Mesh);
-            MatGround.AddMeshMaterial(pipeline, "PineSpawns", Resources.UseMaterial("PineLeaves", HostScript.ModDomain), Renderer.Opaque.Mesh);
-            MatGround.AddMeshMaterial(pipeline, "PineSpawns", Resources.UseMaterial("::Shadow", HostScript.ModDomain), Renderer.Shadow.Mesh);
-
-            return base.init();
-        }
+			pipeline = MatGround.AddPipeline(Resources.UseGeometryPipeline("PineField", HostScript.ModDomain), "Input", 0, 4);
+			MatGround.AddDefaultShadowAndZPre(pipeline, "Input");
+			MatGround.AddMeshMaterial(pipeline, "Input", Resources.UseMaterial("::Terrain/GrassyMountains", HostScript.ModDomain), Renderer.Opaque.Mesh);
+			MatGround.AddMeshMaterial(pipeline, "PineSpawns", Resources.UseMaterial("PineLeaves", HostScript.ModDomain), Renderer.Opaque.Mesh);
+			return base.init();
+		}
 
 		/// <summary>
 		/// Creates the CSG node network for the terrain generation.
 		/// </returns>
 		public override CsgNode createTerrain()
-        {
+		{
 			// Load and return a CsgNode based on the "Hills" expression resource. This will create some generic perlin-based hills.
-            CsgOpUnion union = new CsgOpUnion();
+			CsgOpUnion union = new CsgOpUnion();
 			ExpressionResource expression = Resources.UseExpression("Hills", HostScript.ModDomain);
-            union.AddNode(new CsgExpression(MatGround.MaterialIndex, expression));
-            return union;
-        }
+			union.AddNode(new CsgExpression(MatGround.MaterialIndex, expression));
+			return union;
+		}
 
 	}
 
@@ -78,8 +76,8 @@ namespace UpvoidMiner
 		public static void Startup(IntPtr _unmanagedModule)
 		{
 			// Get and save the resource domain of the mod, needed for loading resources.
-            Mod = Module.FromHandle(_unmanagedModule);
-            ModDomain = Mod.ResourceDomain;
+			Mod = Module.FromHandle(_unmanagedModule);
+			ModDomain = Mod.ResourceDomain;
 
 			// Create the world. Multiple worlds could be created here, but we only want one.
 			// Use the UpvoidMinerWorldGenerator, which will create a simple terrain with some vegetation.
--- Scripts/LocalScript.cs
+++ Scripts/LocalScript.cs
@@ -17,40 +17,39 @@ namespace UpvoidMiner
 	{
 		public static GenericCamera Camera;
 		public static FreeCameraControl CameraControl;
-
 		public static Module Mod;
 		public static ResourceDomain ModDomain;
 
 		/// <summary>
 		/// Updates the camera position.
 		/// </summary>
-        private static void Update(float _elapsedSeconds)
-        {
-            CameraControl.Update(_elapsedSeconds);
-        }
+		private static void Update(float _elapsedSeconds)
+		{
+			CameraControl.Update(_elapsedSeconds);
+		}
 
 		/// <summary>
 		/// Initializes the local part of the mod.
 		/// </summary>
-		public static void Startup (IntPtr _unmanagedModule)
+		public static void Startup(IntPtr _unmanagedModule)
 		{
 			// Create a simple camera that allows free movement.
-			Camera = new GenericCamera ();
-			Camera.FarClippingPlane = 3500.0;
-			CameraControl = new FreeCameraControl (-10f, Camera);
+			Camera = new GenericCamera();
+			Camera.FarClippingPlane = 750.0;
+			CameraControl = new FreeCameraControl(-10f, Camera);
 
 			// Get the world (created by the host script)
-			World world = Universe.GetWorldByName ("UpvoidMinerWorld");
+			World world = Universe.GetWorldByName("UpvoidMinerWorld");
 
 			// Place the camera in the world
-			world.AttachCamera (Camera);
-			if (Rendering.ActiveMainPipeline != null)
-				Rendering.ActiveMainPipeline.SetCamera (Camera);
+			world.AttachCamera(Camera);
+			if(Rendering.ActiveMainPipeline != null)
+				Rendering.ActiveMainPipeline.SetCamera(Camera);
 
-			GC.KeepAlive (Camera);
+			GC.KeepAlive(Camera);
 
 			// Configure the camera to receive user input
-			Input.RootGroup.AddListener (CameraControl);
+			Input.RootGroup.AddListener(CameraControl);
 
 			// Registers the update callback that updates the camera position.
 			Scripting.RegisterUpdateFunction(Update, 1 / 60.0f, 3 / 60.0f);
